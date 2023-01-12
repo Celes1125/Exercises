9b95ff68e808100bb152a27106445ac073edb4d6
cc <colauttiadad@gmail.com>
2023-01-12 14:47:09 +0100

    just refreshing

---
+++ src/UsingNumberFormat.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class UsingNumberFormat {
+    public static void main(String[] args) {
+        // formating a number out
+        Double number = Double.parseDouble(JOptionPane.showInputDialog("Enter a double number"));
+        System.out.printf("%1.2f",number );
+
+    }
+}
