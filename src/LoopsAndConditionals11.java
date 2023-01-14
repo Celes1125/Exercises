cc7528c2ba2f74933b05a18fe0664e5c168b20b1
cc <colauttiadad@gmail.com>
2023-01-14 17:34:36 +0100

    agrego los ejercicios de sentencias condicionales e iterativas del curso de Global Academy

---
+++ src/LoopsAndConditionals11.java
@@ -0,0 +1,41 @@
+public class LoopsAndConditionals11 {
+
+
+    public static void main(String[] args) {
+        // el ejercicio de los triángulos
+
+        traingles();
+    }
+
+    private static void traingles() {
+        for (int i = 0; i <7 ; i++) {
+            String mensaje = " ";
+            for (int j = 0; j < i+1 ; j++) {
+                mensaje = mensaje + "*";
+            }
+            System.out.println(mensaje);
+
+        }
+
+        for (int i = 0; i <7 ; i++) {
+            String mensaje = " ";
+            for (int j = 0; j < 7-i ; j++) {
+                mensaje = mensaje + "*";
+            }
+            System.out.println(mensaje);
+
+        }
+
+
+        for (int i = 0; i < 7; i++) {
+            String mensaje = "";
+            for (int j = 0; j < 6 - i; j++) {
+                mensaje = mensaje + " ";
+            }
+            for (int j = 0; j < i + 1; j++) {
+                mensaje = mensaje + "*";
+            }
+            System.out.println(mensaje);
+        }
+    }
+}
