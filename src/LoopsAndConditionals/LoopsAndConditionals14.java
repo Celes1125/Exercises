77f9ec792772676e7870e00945819b5fb7236be7
cc <colauttiadad@gmail.com>
2023-01-21 15:58:40 +0100

    refreshing

--- src/LoopsAndConditionals/LoopsAndConditionals14.java
+++ src/LoopsAndConditionals/LoopsAndConditionals14.java
@@ -10,17 +10,14 @@ public class LoopsAndConditionals14 {
         int enteredNumber = 77;
         int attempts= 0;
 
-        while (randomNumber!=enteredNumber){
+        do {
             enteredNumber= Integer.parseInt(JOptionPane.showInputDialog("Guess the random number!"));
             attempts++;
-
             if(randomNumber>enteredNumber){
                 System.out.println("The random number is greater than the one you've entered");
             }else if (randomNumber<enteredNumber){
                 System.out.println("The random number is less than the one you've entered");
-            }
-
-        }
+            }}while (randomNumber!=enteredNumber);
 
         System.out.println("Congrats! You've guess the random number in " + attempts +" attemps");
 
