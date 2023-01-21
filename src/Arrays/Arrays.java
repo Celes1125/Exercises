725f1dbbfe760ed37c2ad06eb6bfc6be2c2e1e1b
cc <colauttiadad@gmail.com>
2023-01-21 19:11:34 +0100

    refreshing

---
+++ src/Arrays/Arrays.java
@@ -0,0 +1,31 @@
+public class Arrays {
+    public static void main(String[] args) {
+        int [] myArray = new int[5];
+        myArray[0]=50;
+        myArray[1]=60;
+        myArray[2]=70;
+        myArray[3]=80;
+        myArray[4]=90;
+
+        System.out.println(myArray[0]);
+        for (int i = 0; i < 5; i++) {
+            System.out.println(myArray[i]);
+        }
+
+        int [] myArray2 = {50,60,70,80,90};
+        for (int i = 0; i < 5; i++) {
+            System.out.println(myArray[i]);
+        }
+
+        int [] myArray3 = {5,6,7,8,9, 10, 11, 12, 13, 15, 19};
+        System.out.println(myArray3.length);
+        for (int i = 0; i < myArray3.length; i++) {
+            System.out.println("The index " + i + " contains " + myArray3[i]);
+
+        }
+
+    }
+
+
+
+}
---
+++ src/Maths/Factorial.java
@@ -0,0 +1,17 @@
+package Maths;
+
+import javax.swing.*;
+
+//calculating factorial
+public class Factorial {
+    public static void main(String[] args) {
+        long result = 1L;
+        int number = Integer.parseInt(JOptionPane.showInputDialog("Please enter a number"));
+        for (int i = number; i > 0 ; i--) {
+            result = result *i;
+
+        }
+        System.out.println("The factorial o the number is: " + result);
+
+    }
+}
